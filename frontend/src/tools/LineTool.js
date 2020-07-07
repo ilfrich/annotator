64f1a979c3e8d7dddaed91022ae42f2134f3795c
Peter Ilfrich <peter.ilfrich@au1.ibm.com>
2020-07-07 22:54:52 +1000

    Reduced code for line tool, as it is pretty much a polygon tool

--- frontend/src/tools/LineTool.js
+++ frontend/src/tools/LineTool.js
@@ -1,6 +1,6 @@
 import React from "react"
-import common from "./common"
 import mixins from "../mixins"
+import PolygonTool from "./PolygonTool"
 
 class LineToolOptionPanel extends React.Component {
     constructor(props) {
@@ -25,17 +25,7 @@ class LineToolOptionPanel extends React.Component {
     }
 }
 
-class LineTool extends common.CommonTool {
-    constructor(toolId, canvas, zoom, redraw, add) {
-        super(toolId, canvas, zoom, redraw, add)
-        this.currentPolygon = null
-        this.optionPanel = null
-    }
-
-    getCursor() {
-        return "crosshair"
-    }
-
+class LineTool extends PolygonTool {
     getOptionPanel() {
         const handleSaveEvent = () => {
             this.add([...this.currentPolygon])
@@ -52,37 +42,8 @@ class LineTool extends common.CommonTool {
         )
     }
 
-    handleClick(ev) {
-        const mousePos = this.getMousePos(ev)
-        if (this.currentPolygon == null) {
-            // create initial polygon
-            this.currentPolygon = [[mousePos.x, mousePos.y]]
-            this.redraw()
-            return
-        }
-
-        this.currentPolygon.push([mousePos.x, mousePos.y])
-        this.redraw()
-    }
-
-    redraw() {
-        this.redrawParent()
-
-        this.optionPanel.setState({
-            currentPolygon: this.currentPolygon,
-        })
-
-        if (this.currentPolygon != null) {
-            if (this.currentPolygon.length === 1) {
-                // only one entry, just draw a single dot
-                this.ctx.fillStyle = "#0f0"
-                this.ctx.fillRect(this.currentPolygon[0][0] * this.zoom, this.currentPolygon[0][1] * this.zoom, 1, 1)
-                return
-            }
-
-            // draw polygon lines of current polygon
-            this.drawPolygon(this.currentPolygon, false)
-        }
+    drawPolygon(polygon, close = true) {
+        super.drawPolygon(polygon, false)
     }
 }
 
